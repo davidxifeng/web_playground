d8a38871e9a5f2f2c9ebd86cdf70fe83b55683ee
david feng <davidxifeng@gmail.com>
2013-01-03 00:38:34 +0800

    学会初步使用了heist在happstack中

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,55 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Main where
 import Control.Monad
-import Control.Monad.Trans
+-- import Control.Monad.Trans
 import Happstack.Server
 
-import Text.Blaze.Html5 (Html(), (!), toHtml, a, p,  input)
+import Text.Blaze.Html5 (Html(), (!), toHtml, a, p)--input
 import qualified Text.Blaze.Html5 as H
 
 import Text.Blaze.Html5.Attributes (href)
 import qualified Text.Blaze.Html5.Attributes as A
 
-import Data.Text.Lazy (unpack)
-import Data.Text (Text)
 
-import TestAcid
+--import Data.Text.Lazy (unpack)
+
+-- temp for heist test
+--import Data.Text (Text)
+import qualified Data.Text as T
+import Control.Monad.Trans    (MonadIO)
+
+import Happstack.Server.Heist (templateReloader, templateServe, render)
+import Text.Templating.Heist  (HeistT, Template, HeistState,
+        bindSplice, bindSplices, defaultHeistState, getParamNode)
+import Text.Templating.Heist.TemplateDirectory (newTemplateDirectory')
+
+import qualified Text.XmlHtml as X
+
+
+factSplice :: (Monad m) => HeistT m Template
+factSplice = do
+  input <- getParamNode
+  let text = T.unpack $ X.nodeText input
+      n    = read text :: Int
+  return [X.TextNode $ T.pack $ show $ product [1..n]]
+
+templateState :: (MonadIO m) => HeistState m
+templateState = bindSplice (T.pack "fact") factSplice defaultHeistState
+
+mySplice1 :: (Monad m) => HeistT m Template
+mySplice1 = do
+  return [X.TextNode $ T.pack "david happy feng"]
+
+mySplice2 :: (Monad m) => HeistT m Template
+mySplice2 = do
+  _ <- getParamNode
+  return [X.TextNode $ T.pack "I am david"]
+
+mySplice3 :: (Monad m) => HeistT m Template
+mySplice3 = do
+  --input <- getParamNode
+  return [X.Element (T.pack "strong")([])([X.TextNode $ T.pack "I am a text node"])]
+
 
 davidConf :: Conf
 davidConf = nullConf {port = 80}
@@ -32,7 +68,21 @@ handlers = do
 
 
 main :: IO()
-main = simpleHTTP davidConf handlers
+main = do
+    td <- newTemplateDirectory' "." (templateState)
+    td2 <- newTemplateDirectory' "." (bindSplices ss defaultHeistState)
+    simpleHTTP davidConf $ msum 
+        [ dir "jack" $ templateServe td
+        , dir "j2" $ templateServe td2
+        --, dir "reload" $ nullDir >> templateReloader td
+        --, nullDir >> seeOther ("/factorial":: String) (toResponse ())
+        ]
+    --simpleHTTP davidConf handlers
+    where
+    ss = [(T.pack "who", mySplice1)
+         ,(T.pack "me", mySplice2)
+         ,(T.pack "you", mySplice3)
+         ]
 
 myform :: ServerPart Response
 myform = msum [viewForm, processForm]
@@ -56,12 +106,12 @@ myform = msum [viewForm, processForm]
 
 mycookie :: ServerPart Response
 mycookie = 
-    msum [ do rq <- askRq
+    msum [ -- do rq <- askRq
               --liftIO $ print (rqPaths rq)
               --this line will failed after stdout not available after
               --logout
-              mzero
-         , do (requests::Int) <- readCookieValue "requests"
+              --mzero ,
+           do (requests::Int) <- readCookieValue "requests"
               addCookie Session (mkCookie "requests" (show (requests + 1)))
               ok $ template "cookie" $ do
                         p $ toHtml (("you have made " ++ show requests ++ " requests to this page")::String)
@@ -71,7 +121,7 @@ mycookie =
          ]
 
 
-template :: Text -> Html -> Response
+template :: T.Text -> Html -> Response
 template title bd = toResponse $
         H.html $ do
           H.head $ do
