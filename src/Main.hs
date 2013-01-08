67eda906afab585e6f201a9cf48c16d35e521baa
david feng <davidxifeng@gmail.com>
2013-01-08 13:16:08 +0800

    test update post receive hook

--- src/Main.hs
+++ src/Main.hs
@@ -225,7 +225,7 @@ echo' = path $ \(msg :: String) ->
                 p $ "echo: " >> toHtml msg
                 p "change to see something else"
                 p "测试使用authbind后的post hook"
-                p "再次测试"
+                p "再次测试,try use exec fix zoombie process"
 
 myFiles :: ServerPart Response
 myFiles = do
